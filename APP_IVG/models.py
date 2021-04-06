8d85f92b6af54788596a34295fd494fb74e8299e
Sirmadeira <71156378+Sirmadeira@users.noreply.github.com>
2021-04-06 11:55:07 -0300

    Inicializando dbs

--- APP_IVG/models.py
+++ APP_IVG/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Dados(models.Model):
+	data_postada = models.DateTimeField(default=timezone.now)
+	autor= models.ForeignKey(User, on_delete=models.CASCADE)

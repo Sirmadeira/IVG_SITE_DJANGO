c029417f2affc23bcfbd8bd1f71a55c347c9b030
Sirmadeira <71156378+Sirmadeira@users.noreply.github.com>
2021-04-06 15:00:57 -0300

    Adicionando formularios

--- usuarios/views.py
+++ usuarios/views.py
@@ -1,6 +1,14 @@
-from django.shortcuts import render
-from django.contrib.auth.forms import UserCreationForm
-
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from .forms import UserCadastroForm
 def Cadastro(request):
-	form = UserCreationForm()
+	if request.method == 'POST':
+		form = UserCadastroForm(request.POST)
+		if form.is_valid():
+			form.save()
+			username= form.cleaned_data.get('username')
+			messages.success(request, f'Conta criada para {username}!')
+			return redirect('APP_IVG-Homepage')
+	else:
+		form = UserCadastroForm()
 	return render(request, 'usuarios/Cadastro.html', {'form': form})

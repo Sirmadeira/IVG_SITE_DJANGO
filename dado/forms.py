a34c6d457521c565c196e6afa0cedd7c6d854a8c
Sirmadeira <71156378+Sirmadeira@users.noreply.github.com>
2021-04-11 10:34:35 -0300

    Update forms.py

--- dado/forms.py
+++ dado/forms.py
@@ -1,9 +1,8 @@
 from django import forms
-from django.forms import ModelForm
 from .models import DadoDB
 
 
-class InsiraDadosForm(ModelForm):
+class InsiraDadosForm(forms.ModelForm):
 	class Meta:
 		model = DadoDB
 		fields = ['marca','modelo','ano','status','cor','combustivel','quilometragem','lucro','preco','margem_de_lucro']

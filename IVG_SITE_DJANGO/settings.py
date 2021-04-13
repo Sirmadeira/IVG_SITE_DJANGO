302a7a02181bb9a5d11d9c0fa47c0ec04c8845d6
brenoakihiromorimoto <akihiro.arq@gmail.com>
2021-04-13 20:24:24 -0300

    o

--- IVG_SITE_DJANGO/settings.py
+++ IVG_SITE_DJANGO/settings.py
@@ -126,7 +126,7 @@ STATIC_URL = '/static/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
-CRISPY_TEMPLATE_PACK = 'bootstrap4' 
+CRISPY_TEMPLATE_PACK = 'bootstrap5' 
 
 LOGIN_REDIRECT_URL = 'APP_IVG-Homepage'
 LOGIN_URL = 'Login'

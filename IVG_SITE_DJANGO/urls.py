573e9510d7c225147056812f2219f1d1d041ab44
Sirmadeira <71156378+Sirmadeira@users.noreply.github.com>
2021-04-22 13:00:34 -0300

    Configuracao de email pessoal.

--- IVG_SITE_DJANGO/urls.py
+++ IVG_SITE_DJANGO/urls.py
@@ -33,7 +33,10 @@ urlpatterns = [
     #Essas views estao em minusculo porque django e chato, e nao aceita views de reset com primeira letra maiuscula
      path('password-reset/',
          auth_views.PasswordResetView.as_view(
-             template_name='usuarios/password_reset.html'
+            # Templates para o email de reset de usuarios
+             template_name='usuarios/password_reset.html',
+             email_template_name='usuarios/password_reset_email.html',
+             subject_template_name='usuarios/password_reset_subject.txt'
          ),
          name='password_reset'),
     path('password-reset/done/',

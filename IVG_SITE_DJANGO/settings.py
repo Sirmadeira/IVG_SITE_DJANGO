9e73455c487024e1d3c4a2256c86e47ec6415369
Sirmadeira <71156378+Sirmadeira@users.noreply.github.com>
2021-04-28 13:12:18 -0300

    Desfazendo a autenticacao de email!

--- IVG_SITE_DJANGO/settings.py
+++ IVG_SITE_DJANGO/settings.py
@@ -138,6 +138,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST ='smtp.gmail.com'
 EMAIL_PORT ='587'
 EMAIL_USE_TLS=True
-DEFAULT_FROM_EMAIL= os.environ.get('MAIL_USERNAME')
 EMAIL_HOST_USER = os.environ.get('MAIL_USERNAME')
 EMAIL_HOST_PASSWORD = os.environ.get('MAIL_PASSWORD')
--- usuarios/views.py
+++ usuarios/views.py
@@ -1,8 +1,6 @@
-import os
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
-from django.core.mail import EmailMessage
 from .forms import UserCadastroForm, UserUpdateForm, EmpresaUpdateForm
 
 
@@ -10,20 +8,8 @@ def Cadastro(request):
 	if request.method == 'POST':
 		form = UserCadastroForm(request.POST)
 		if form.is_valid():
-			submitbutton= request.POST.get("submit")
-			email_subject = 'Ative sua conta!'
-			email_body ='batata'
-			email_sender = os.environ.get('MAIL_USERNAME')
-			email_usuario = form.cleaned_data.get('email')
-			email_verif= EmailMessage(
-				email_subject,
-				email_body,
-				email_sender,
-				[email_usuario],
-				)
-			email_verif.send(fail_silently=False)
 			form.save()
-			messages.success(request, f'Sua conta foi criada com sucesso!Favor verificar seu email para poder ter acesso!')
+			messages.success(request, f'Sua conta foi criada com sucesso!Favor aguardar enquanto a gente verifica se você é um parceiro!')
 			return redirect('Login')
 	else:
 		form = UserCadastroForm()

44b0e4c3a5e5480cdbb24e42724b31f686fb2c8f
Sirmadeira <71156378+Sirmadeira@users.noreply.github.com>
2021-04-29 11:40:20 -0300

    Melhorando a pasta admin atraves de user auth

--- usuarios/admin.py
+++ usuarios/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
+from django.contrib.auth import admin as auth_admin
 from .models import CustomUser,EmpresaDB
 
-admin.site.register(CustomUser)
+admin.site.register(CustomUser, auth_admin.UserAdmin)
 admin.site.register(EmpresaDB)
 
 # Register your models here.

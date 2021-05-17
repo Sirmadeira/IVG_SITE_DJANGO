bc0f83c2728c42aa6bbc9ec2010c56fb12c88556
Sirmadeira <71156378+Sirmadeira@users.noreply.github.com>
2021-05-17 14:19:51 -0300

    Pequenas alteracoes apra deixar a admin com um semi crud

--- data/admin.py
+++ data/admin.py
@@ -2,5 +2,10 @@ from django.contrib import admin
 from .models import DataDB
 
 
-admin.site.register(DataDB)
-# Register your models here.
+admin.site.site_header= 'Admin data vendas'
+
+class DataDBAdmin(admin.ModelAdmin):
+	list_display= ('marca','modelo','motor','ano','status','cor','localidade','combustivel','quilometragem','preco','lucro','autor','data_postada')
+	list_filter= ('data_postada',)
+
+admin.site.register(DataDB,DataDBAdmin)
--- datac/admin.py
+++ datac/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
 from .models import DataDBC
 
+admin.site.site_header= 'Admin data compras'
 
-admin.site.register(DataDBC)
+
+class DataDBAdmin(admin.ModelAdmin):
+	list_display= ('marca','modelo','motor','ano','status','cor','localidade','combustivel','quilometragem','preco','autor','data_postada')
+	list_filter= ('data_postada',)
+admin.site.register(DataDBC, DataDBAdmin)
